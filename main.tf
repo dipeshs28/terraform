2e2a1721e420eb24c2bdfc66bd21640e1877b1e2
dipeshs28 <127583985+dipeshs28@users.noreply.github.com>
2023-10-10 17:41:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "one" {
   instance_type   = "t2.micro"
   key_name        = "jenkins1"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-south-1a"
+  availability_zone = "ap-south-1b"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
@@ -46,7 +46,7 @@ resource "aws_instance" "three" {
   instance_type   = "t2.micro"
   key_name        = "jenkins1"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-south-1a"
+  availability_zone = "ap-south-1b"
   tags = {
     Name = "app-server-1"
   }
@@ -103,7 +103,7 @@ default = ["user1", "user2", "user3", "user4"]
 }
 
 resource "aws_ebs_volume" "eight" {
- availability_zone = "ap-south-1a"
+ availability_zone = "ap-south-1b"
   size = 40
   tags = {
     Name = "ebs-001"
